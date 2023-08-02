b65b70ed5250b95d92b5fe929a6608ff2255a767
Allan Okoth <allanokothdev@gmail.com>
2023-08-03 01:57:27 +0200

    Marketplace Functions

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::{near_bindgen, PanicOnDefault};
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+
+pub struct Marketplace {
+    products: UnorderedMap<String, String>,
+}
+
+#[near_bindgen]
+impl Marketplace {
+    //  Marketplace methods implementation
+
+    #[init]
+    pub fn init() -> Self {
+        Self {
+            products: UnorderedMap::new(b"product".to_vec()),
+        }
+    }
+
+    pub fn set_product(&mut self, id: String, product_name: String) {
+        self.products.insert(&id, &product_name);
+    }
+
+    pub fn get_product(&self, id: &String) -> Option<String> {
+        self.products.get(id)
+    }
+}
